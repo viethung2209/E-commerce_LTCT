3ff4e18abea87e5842cbc491785f8f0f2dc60ee7
diepanhng0711 <diepanhng0711@gmail.com>
2023-02-22 14:23:44 +0700

    add cart api _ sp15

--- src/Api/cart.api.js
+++ src/Api/cart.api.js
@@ -1,6 +1,19 @@
 import axios from "axios";
 
+//SP_11
 export const addCart = async (request) => {
-    //post api with json body
+    
     return await axios.post("https://sp11-cart.000webhostapp.com/api/carts_trans/add-to-cart.php", request)
 }
+
+
+//SP_15
+export const addCart15 = async (request) => {
+
+}
+
+
+
+
+
+
--- src/Component/Home/Products.jsx
+++ src/Component/Home/Products.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { addCart } from '../../Api/cart.api';
+import { showLogin } from '../../Redux/auth.slice';
 
 const Products = ({ productList }) => {
+  const currentUser = useSelector(state => state.auth.login.currentUser);
+  const dispatch = useDispatch();
+
+  async function addToCart(productId) {
+    if (!currentUser) {
+      dispatch(showLogin());
+    }
+
+    console.log(productId)
+    let request = {
+      user_id: currentUser.id,
+      product_id: productId,
+      quantity: 1,
+    }
+    await addCart(JSON.stringify(request));
+    console.log("Data returned")
+    console.log("Add to Cart")
+  }
+
   return (
     <div className="container-fluid pt-5">
       <div className="text-center mb-4">
@@ -13,7 +35,7 @@ const Products = ({ productList }) => {
             <div className="card product-item border-0 mb-4">
               <div
                 className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                {/* <img className="img-fluid w-100" src={product.sub_products[0].image_url} alt="" /> */}
+                <img className="img-fluid w-100" src={product.sub_products.length !== 0? product.sub_products[0].image_url : null} alt="" />
               </div>
               <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
                 <h6 className="text-truncate mb-3">{product.name}</h6>
@@ -27,7 +49,7 @@ const Products = ({ productList }) => {
               <div className="card-footer d-flex justify-content-between bg-light border">
                 <Link to={`/product-detail/${product.id}`} className="btn btn-sm text-dark p-0"><i
                   className="fas fa-eye text-primary mr-1"></i>Xem chi tiết</Link>
-                <Link className="btn btn-sm text-dark p-0"><i
+                <Link to="/pages/shopping-card" onClick={() => addToCart(product.id)}  className="btn btn-sm text-dark p-0"><i
                   className="fas fa-shopping-cart text-primary mr-1"></i>Thêm vào giỏ hàng</Link>
               </div>
             </div>
